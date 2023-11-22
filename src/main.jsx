85e34d1c2d09fbd40c6f0479f65d300296b64428
GustavoT00 <gustavo_ale.tovar@uao.edu.co>
2023-11-22 15:53:41 -0500

    Tarjeta Propiedad

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './pages/Home/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
