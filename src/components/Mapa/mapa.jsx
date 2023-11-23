417a1c43dc66709d236e5b91a621aea233b3ebc0
GustavoT00 <gustavo_ale.tovar@uao.edu.co>
2023-11-23 10:53:45 -0500

    Actualizaciones

--- src/components/Mapa/mapa.jsx
+++ src/components/Mapa/mapa.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import useGoogleMaps from '../Hooks/useGoogleMaps';
+import React, { useEffect } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
-const Mapa = () => {
-  useGoogleMaps('TU_API_KEY'); // Reemplaza 'TU_API_KEY' con tu propia clave de API
+function MapContainer() {
+  useEffect(() => {
 
-  return (
-    <div id="map" style={{ width: '100%', height: '400px' }}></div>
-  );
+    const map = L.map("map").setView([7.067, -73.867], 13);
+
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+      maxZoom: 18,
+    }).addTo(map);
+
+    const newCharacterIcon = L.icon({
+      iconUrl:
+        "https://pngimg.com/d/gps_PNG18.png", 
+      iconSize: [60, 40], 
+      iconAnchor: [25, 50], 
+    });
+
+    // agrega el nuevo marcador del personaje al mapa
+    L.marker([7.067, -73.867], { icon: newCharacterIcon }).addTo(map);
+  }, []);
+
+  return <div id='map' style={{ width: "100%", height: "100vh" }}></div>;
 }
 
-export default Mapa;
+export default MapContainer;
--- src/pages/DetallesPropiedad/propiedaddes.jsx
+++ src/pages/DetallesPropiedad/propiedaddes.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
+import React, { useState } from 'react';
+import Header from '../../components/Header/Header';
+import PropertyDetails from '../../components/PropertyDetails/PropertyDetails';
+import ReservationForm from '../../components/ReservationForm/ReservationForm';
+import LoginModal from '../../components/LoginModal/LoginModal';
 
-const Home = () => {
-  
-    return (
-      <div className="home-container">
-        <Header />
+const PropertyPage = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    // Lógica de autenticación
+    // Actualiza el estado de isLoggedIn cuando el usuario inicia sesión
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <div>
+      <Header isLoggedIn={isLoggedIn} />
+      <div className="container mt-5">
+        <h2>Nombre de la Propiedad</h2>
+        {/* Fotos de la propiedad */}
+        <div className="property-photos">
+          {/* Aquí van las fotos de la propiedad */}
+        </div>
+
+        {/* Descripción de la propiedad */}
+        <PropertyDetails
+          pricePerHour="$50"
+          address="Calle Principal 123"
+          locationMap={{ lat: 7.067, lng: -73.867 }}
+        />
+
+        {/* Formulario de reserva */}
+        {isLoggedIn ? (
+          <ReservationForm />
+        ) : (
+          <p>Debes iniciar sesión para hacer una reserva.</p>
+        )}
       </div>
-    );
-  }
-  export default Home;
+
+      {/* Modal de Login */}
+      <LoginModal onLogin={handleLogin} />
+    </div>
+  );
+};
+
+export default PropertyPage;
